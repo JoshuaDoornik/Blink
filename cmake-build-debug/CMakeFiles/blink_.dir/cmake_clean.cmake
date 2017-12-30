2298fb01469395768f4ee8ab505d314813e0a8b1
Josh <jddoornik@outlook.com>
2017-12-30 13:59:15 +0100

    init, with C files this time

---
+++ cmake-build-debug/CMakeFiles/blink_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blink_.dir/main.c.o"
+  "blink_.pdb"
+  "blink_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/blink_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
