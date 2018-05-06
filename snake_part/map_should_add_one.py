1e1fcdb677681c2157d8a599688ef65094964de9
Josh <jddoornik@outlook.com>
2018-05-06 17:47:30 +0200

    can now call CUDA code from python. using insane disutils workaround. be sure to set /usr/local/cuda

--- snake_part/map_should_add_one.py
+++
@@ -1,8 +0,0 @@
-#Though it looks like an ordinary python import, the addList module is implemented in C
-import blink
-
-def plus_one(i):
-    return i+1
-
-l = [1,2,3,4,5]
-print blink.own_map(l,plus_one)
